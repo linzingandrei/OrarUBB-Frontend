779b4ce66549bf23691389817cf6e8e1a9e0e0f8
sindrila <mihail.mindrila@stud.ubbcluj.ro>
2024-11-12 02:06:59 +0200

    Made navbar responsive, made LandingPage.jsx and InformatiiSali.jsx responsive w.r.t. navbar.

--- orarubb-web_frontend/src/pages/LandingPage/LandingPage.jsx
+++ orarubb-web_frontend/src/pages/LandingPage/LandingPage.jsx
@@ -5,45 +5,48 @@ import Button from "../../components/button/Button";
 const LandingPage = () => {
   return (
     <Layout>
-      <div className="main-content">
-        <h2 className="title">Orar Facultatea de Matematica si Informatica</h2>
-        <p className="subtitle">Anul 2024-2025, Semestrul I</p>
-        <div className="buttons-container">
-          <Button
-            color="primary"
-            label="Orarul Studentilor"
-            className="medium rounded"
-          />
-          <Button
-            color="primary"
-            label="Orarul Cadrelor Didactice"
-            className="medium rounded"
-          />
-          <Button
-            color="primary"
-            label="Orarul Disciplinelor"
-            className="medium rounded"
-          />
-          <Button
-            color="primary"
-            label="Informatii sali"
-            className="medium rounded"
-            to="/informatii-sali"
-          />
+        <div className="background-wrapper">
+            <div className="main-content">
+                <h2 className="title">Orar Facultatea de Matematica si Informatica</h2>
+                <p className="subtitle">Anul 2023-2024, Semestrul II</p>
+                <div className="buttons-container">
+                    <Button
+                        color="primary"
+                        label="Orarul Studentilor"
+                        className="medium rounded"
+                    />
+                    <Button
+                        color="primary"
+                        label="Orarul Cadrelor Didactice"
+                        className="medium rounded"
+                    />
+                    <Button
+                        color="primary"
+                        label="Orarul Disciplinelor"
+                        className="medium rounded"
+                    />
+                    <Button
+                        color="primary"
+                        label="Informatii sali"
+                        className="medium rounded"
+                        to="/informatii-sali"
+                    />
+                </div>
+                <div className="footer">
+                    <p>
+                        Orar curs facultativ transversal Fundamente de educatie umanista.
+                        Teoria argumentarii - MIERCURI 18-20, online, MSTeams.
+                    </p>
+                    <p>Mai multe informatii gasiti aici.</p>
+                    <p>
+                        Orarul de Limba spaniola si Limba italiana din cadrul
+                        Departamentului de Limbi Străine Specializate.
+                    </p>
+                    <p>Orar Educatie Fizica, sem. 1, an univ. 2024-2025.</p>
+                </div>
+            </div>
         </div>
-        <div className="footer">
-          <p>
-            Orar curs facultativ transversal Fundamente de educatie umanista.
-            Teoria argumentarii - MIERCURI 18-20, online, MSTeams.
-          </p>
-          <p>Mai multe informatii gasiti aici.</p>
-          <p>
-            Orarul de Limba spaniola si Limba italiana din cadrul
-            Departamentului de Limbi Străine Specializate.
-          </p>
-          <p>Orar Educatie Fizica, sem. 1, an univ. 2024-2025.</p>
-        </div>
-      </div>
+
     </Layout>
   );
 };
