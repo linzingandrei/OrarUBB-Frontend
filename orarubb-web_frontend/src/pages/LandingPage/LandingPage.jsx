ec535b4676cc9cb4ac6d89d2d47d19642a297e2e
Alle43221 <allemoroz14@gmail.com>
2024-12-04 20:58:56 +0200

    Refined LandingPage.jsx, moved text from footer into buttons, started working on the footer, ensured that the background covers the entire height of the screen at any time. Added diacritics.

--- orarubb-web_frontend/src/pages/LandingPage/LandingPage.jsx
+++ orarubb-web_frontend/src/pages/LandingPage/LandingPage.jsx
@@ -7,12 +7,12 @@ const LandingPage = () => {
     <Layout>
         <div className="background-wrapper">
             <div className="main-content">
-                <h2 className="title">Orar Facultatea de Matematica si Informatica</h2>
-                <p className="subtitle">Anul 2023-2024, Semestrul II</p>
+                <h2 className="title">Orar Facultatea de Matematică și Informatică</h2>
+                <p className="subtitle">Anul 2024-2025, Semestrul II</p>
                 <div className="buttons-container">
                     <Button
                         color="primary"
-                        label="Orarul Studentilor"
+                        label="Orarul Studenților"
                         className="medium rounded"
                         to="/study-programs"
                     />
@@ -30,22 +30,41 @@ const LandingPage = () => {
                     />
                     <Button
                         color="primary"
-                        label="Informatii sali"
+                        label="Informații Săli"
                         className="medium rounded"
                         to="/informatii-sali"
                     />
+
+                    {/* Line Separator */}
+                    <div className="line-separator"></div>
+
+                    <Button
+                        color="primary"
+                        label="Orarul de Limba spaniolă și Limba italiană din cadrul
+                        Departamentului de Limbi Străine Specializate."
+                        className="medium rounded"
+                        to="/"
+                    />
+
+                    <Button
+                        color="primary"
+                        label="Orar Educație Fizică, sem. 2, an univ. 2024-2025."
+                        className="medium rounded"
+                        to="/"
+                    />
+
+                    <p className="custom-button-like">
+                        Orar curs facultativ transversal Fundamente de educație umanistă.<br/>
+                        Teoria argumentării - MIERCURI 18-20, online, MSTeams.
+                        <br/>
+                        Mai multe informații găsiți{" "}
+                        <a href="https://www.cs.ubbcluj.ro/~dana/TArgumentarii/" target="_blank" rel="noopener noreferrer">aici</a>.
+                    </p>
                 </div>
+
                 <div className="footer">
-                    <p>
-                        Orar curs facultativ transversal Fundamente de educatie umanista.
-                        Teoria argumentarii - MIERCURI 18-20, online, MSTeams.
-                    </p>
-                    <p>Mai multe informatii gasiti aici.</p>
-                    <p>
-                        Orarul de Limba spaniola si Limba italiana din cadrul
-                        Departamentului de Limbi Străine Specializate.
-                    </p>
-                    <p>Orar Educatie Fizica, sem. 1, an univ. 2024-2025.</p>
+                <p>Made with 🖤 by.. link-uri github?</p>
+                    <p>© Universitatea Babeş-Bolyai, 2024. Toate drepturile rezervate.</p>
                 </div>
             </div>
         </div>
