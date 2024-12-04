3e5d80e1371c1d334882be4e8c4273a34a877b07
Alle43221 <allemoroz14@gmail.com>
2024-12-04 20:01:14 +0200

    Solved blank space after navbar, started working on the responsive pages

--- orarubb-web_frontend/src/components/header/Navbar.jsx
+++ orarubb-web_frontend/src/components/header/Navbar.jsx
@@ -12,25 +12,27 @@ const Navbar = () => {
 
     return (
         <div>
-            <nav className="navbar">
-                <div className="navbar__logo" title="Faculty Logo" onClick={() => navigate("/")}></div>
-                <div className="navbar__left">
-                    <div className="navbar__language navbar__romanian-lang" title="Romanian Lang Icon"></div>
-                    <div className="navbar__language navbar__english_lang" title="English Lang Icon"></div>
-                </div>
+            <div className="navbar__empty_space">
+                <nav className="navbar">
+                    <div className="navbar__logo" title="Faculty Logo" onClick={() => navigate("/")}></div>
+                    <div className="navbar__left">
+                        <div className="navbar__language navbar__romanian-lang" title="Romanian Lang Icon"></div>
+                        <div className="navbar__language navbar__english_lang" title="English Lang Icon"></div>
+                    </div>
 
 
-                <div className="navbar__right">
-                    {isLoggedIn ? (<>
-                        <button className="navbar__link" onClick={() => navigate("/my-schedule")}>Orarul meu</button>
-                        <button className="navbar__link" onClick={handleLoginClick}>
-                            Log Out <div className="navbar__icon logout-icon" title="Log Out"></div>
-                        </button>
-                    </>) : (<button className="navbar__link" onClick={handleLoginClick}>
-                        Log In <div className="navbar__icon login-icon" title="Log In"></div>
-                    </button>)}
-                </div>
-            </nav>
+                    <div className="navbar__right">
+                        {isLoggedIn ? (<>
+                            <button className="navbar__link" onClick={() => navigate("/my-schedule")}>Orarul meu</button>
+                            <button className="navbar__link" onClick={handleLoginClick}>
+                                Log Out <div className="navbar__icon logout-icon" title="Log Out"></div>
+                            </button>
+                        </>) : (<button className="navbar__link" onClick={handleLoginClick}>
+                            Log In <div className="navbar__icon login-icon" title="Log In"></div>
+                        </button>)}
+                    </div>
+                </nav>
+            </div>
         </div>
     );
 };
