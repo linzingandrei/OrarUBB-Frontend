c1e84e762f4e23f7a8f4515c51685c50654d2812
Nistor Bianca <bianca.nistor55@gmail.com>
2024-12-16 13:33:56 +0200

    Uptadets, the stuy prgram page is responsive

--- orarubb-web_frontend/src/main.jsx
+++ orarubb-web_frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.scss";
 import App from "./App.jsx";
 import { store } from "./store";
 import { Provider } from "react-redux";
