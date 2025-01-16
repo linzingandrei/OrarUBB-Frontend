6e33aa305f48e1a280d505713d5a28b9f8627a1c
sindrila <mihail.mindrila@stud.ubbcluj.ro>
2025-01-17 00:04:57 +0200

    Whatever,...

--- orarubb-web_frontend/src/components/crudSchedule/CrudScheduleTable.jsx
+++ orarubb-web_frontend/src/components/crudSchedule/CrudScheduleTable.jsx
@@ -2,10 +2,10 @@ import "./CrudScheduleTable.scss";
 
 const CrudScheduleTable = ({ scheduleData, onEdit, onDelete }) => {
   return (
-    <div className="crud-table-container">
-      <h2 className="crud-table-title">Orarul meu</h2>
-      <table className="crud-table">
-        <thead>
+      <div className="crud-table-container">
+        <h2 className="crud-table-title">Orarul meu</h2>
+        <table className="crud-table">
+          <thead>
           <tr>
             <th>Ziua</th>
             <th>Orele</th>
@@ -17,57 +17,63 @@ const CrudScheduleTable = ({ scheduleData, onEdit, onDelete }) => {
             <th>Cadrul Didactic</th>
             <th>Acțiuni</th>
           </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
           {scheduleData.map((item, index) => (
-            <tr key={index}>
-              <td>{item.class_day}</td>
-              <td>
-                {item.start_hour} - {item.end_hour}
-              </td>
-              <td>
-                {item.frequency === 0
-                  ? "Săptămânal"
-                  : item.frequency === 1
-                  ? "Săptămâna impară"
-                  : "Săptămâna pară"}
-              </td>
-              <td>
-                <a href={`/${item.room}`} className="link">
-                  {item.room}
-                </a>
-              </td>
-              <td>{item.formation}</td>
-              <td>{item.class_type}</td>
-              <td>
-                <a href={`/${item.course_instance_code}`} className="link">
-                  {item.course_instance_code}
-                </a>
-              </td>
-              <td>
-                <a href={`/${item.teacher}`} className="link">
-                  {item.teacher}
-                </a>
-              </td>
-              <td>
-                <button
-                  className="edit-button"
-                  onClick={() => onEdit(index)}
-                >
-                  Editează
-                </button>
-                <button
-                  className="delete-button"
-                  onClick={() => onDelete(index)}
-                >
-                  Șterge
-                </button>
-              </td>
-            </tr>
+              <tr key={item.classId || index}>
+                <td>{item.classDay}</td>
+                <td>
+                  {item.startHour} - {item.endHour}
+                </td>
+                <td>
+                  {item.frequency === 0
+                      ? "Săptămânal"
+                      : item.frequency === 1
+                          ? "Săptămâna impară"
+                          : "Săptămâna pară"}
+                </td>
+                <td>
+                  <a href={`/${item.room}`} className="link">
+                    {item.room}
+                  </a>
+                </td>
+                <td>{item.formation}</td>
+                <td>{item.classType}</td>
+                <td>
+                  <a href={`/${item.courseInstanceCode}`} className="link">
+                    {item.courseInstanceCode}
+                  </a>
+                  {/*
+                  If you also want to show the full course name,
+                  you can add `item.courseInstanceName` if desired:
+                  <br />
+                  <span>{item.courseInstanceName}</span>
+                */}
+                </td>
+                <td>
+                  <a href={`/${item.teacher}`} className="link">
+                    {item.teacher}
+                  </a>
+                </td>
+                <td>
+                  <button
+                      className="edit-button"
+                      onClick={() => onEdit(index)}
+                  >
+                    Editează
+                  </button>
+                  <button
+                      className="delete-button"
+                      onClick={() => onDelete(index)}
+                  >
+                    Șterge
+                  </button>
+                </td>
+              </tr>
           ))}
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
   );
 };
 
